3390433ac9cfecd2404111dbf569050e2ffd7a63
Rahul@kumar <drkagarwal05@gmail.com>
2024-02-10 23:03:33 +0530

    avoid intersecting state

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
 import Player from './components/Player';
 import GameBoard from './components/GameBoard';
 import { useState } from 'react';
+import Log from './components/Log';
 function App() {
-
   const [activePlayer, setActivePlayer] = useState('X');
+  const [gamePlayer, setGamePlayer] = useState([]);
 
-  function handleSelectButton(){
-    setActivePlayer((currActivePlayer)=> (currActivePlayer === 'X' ? "O" : "X"))
+  function handleSelectButton(rowIndex, colIndex) {
+    setActivePlayer((currActivePlayer) =>
+      currActivePlayer === 'X' ? 'O' : 'X'
+    );
+
+    setGamePlayer((prevTurns) => {
+      let currPlayer = 'X';
+      if (prevTurns.length > 0 && prevTurns[0].player === 'X') currPlayer = 'O';
+      const updateBoard = [
+        { square: { row: rowIndex, col: colIndex }, player: currPlayer },
+        ...prevTurns,
+      ];
+      return updateBoard;
+    });
   }
+
   return (
-    <div id="game-container">
-      <ol id="players" className="highlight-player">
-        <Player name="Player-1" symbol="X" isActive={activePlayer === 'X'} />
-        <Player name="Player-2" symbol="O" isActive={activePlayer === 'O'} />
-      </ol>
-      <GameBoard onSelectButton={handleSelectButton} activePlayerSymbol={activePlayer}/>
-    </div>
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player name="Player-1" symbol="X" isActive={activePlayer === 'X'} />
+          <Player name="Player-2" symbol="O" isActive={activePlayer === 'O'} />
+        </ol>
+        <GameBoard
+          onSelectButton={handleSelectButton}
+          turns={gamePlayer}
+        />
+      </div>
+      <Log />
+    </main>
   );
 }
 
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,23 +1,29 @@
-import { useState } from 'react';
-
 const intialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-export default function GameBoard({ onSelectButton, activePlayerSymbol }) {
-  const [gameBoard, setGameBoard] = useState(intialGameBoard);
-  function handleButtonClick(rowIndex, colIndex) {
-    setGameBoard((prevGameBoard) => {
-      const updateBoard = [
-        ...prevGameBoard.map((innerArray) => [...innerArray]),
-      ];
-      updateBoard[rowIndex][colIndex] = activePlayerSymbol;
-      return updateBoard;
-    });
+export default function GameBoard({ onSelectButton, turns }) {
+  // const [gameBoard, setGameBoard] = useState(intialGameBoard);
+  // function handleButtonClick(rowIndex, colIndex) {
+  //   setGameBoard((prevGameBoard) => {
+  //     const updateBoard = [
+  //       ...prevGameBoard.map((innerArray) => [...innerArray]),
+  //     ];
+  //     updateBoard[rowIndex][colIndex] = activePlayerSymbol;
+  //     return updateBoard;
+  //   });
 
-    onSelectButton();
+  //   onSelectButton();
+  // }
+
+  let gameBoard = intialGameBoard;
+
+  for(const turn of turns){
+    const {square, player} = turn
+    const {row, col} = square
+    gameBoard[row][col] = player
   }
   return (
     <ol id="game-board">
@@ -26,7 +32,7 @@ export default function GameBoard({ onSelectButton, activePlayerSymbol }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => handleButtonClick(rowIndex, colIndex)}>
+                <button onClick={() => onSelectButton(rowIndex, colIndex)}>
                   {playerSymbol}
                 </button>
               </li>
