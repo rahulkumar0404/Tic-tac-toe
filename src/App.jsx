6e428cd53cb85e6d5aada586f6a0e1989f43705b
Rahul@kumar <drkagarwal05@gmail.com>
2024-02-11 00:16:11 +0530

    reduce the state management

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,25 @@ import Player from './components/Player';
 import GameBoard from './components/GameBoard';
 import { useState } from 'react';
 import Log from './components/Log';
+
+function derivedActivePlayer(prevTurns) {
+  let currPlayer = 'X';
+  if (prevTurns.length > 0 && prevTurns[0].player === 'X') currPlayer = 'O';
+  return currPlayer
+}
 function App() {
-  const [activePlayer, setActivePlayer] = useState('X');
+  // const [activePlayer, setActivePlayer] = useState('X');
   const [gamePlayer, setGamePlayer] = useState([]);
+  let activePlayer = derivedActivePlayer(gamePlayer);
 
   function handleSelectButton(rowIndex, colIndex) {
-    setActivePlayer((currActivePlayer) =>
-      currActivePlayer === 'X' ? 'O' : 'X'
-    );
+    // setActivePlayer((currActivePlayer) =>
+    //   currActivePlayer === 'X' ? 'O' : 'X'
+    // );
 
     setGamePlayer((prevTurns) => {
-      let currPlayer = 'X';
-      if (prevTurns.length > 0 && prevTurns[0].player === 'X') currPlayer = 'O';
       const updateBoard = [
-        { square: { row: rowIndex, col: colIndex }, player: currPlayer },
+        { square: { row: rowIndex, col: colIndex }, player: activePlayer },
         ...prevTurns,
       ];
       return updateBoard;
@@ -29,12 +34,9 @@ function App() {
           <Player name="Player-1" symbol="X" isActive={activePlayer === 'X'} />
           <Player name="Player-2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
-        <GameBoard
-          onSelectButton={handleSelectButton}
-          turns={gamePlayer}
-        />
+        <GameBoard onSelectButton={handleSelectButton} turns={gamePlayer} />
       </div>
-      <Log />
+      <Log turns={gamePlayer} />
     </main>
   );
 }
